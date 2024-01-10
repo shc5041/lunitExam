3d223017da9b1023e1d633d9a885927e42e499f0
shc78 <shc78@ncsoft.com>
2024-01-10 19:08:11 +0900

    first

---
+++ src/main/java/com/example/lunitexam/LunitExamApplication.java
@@ -0,0 +1,12 @@
+package com.example.lunitexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunitExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunitExamApplication.class, args);
+    }
+}
